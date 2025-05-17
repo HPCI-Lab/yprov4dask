69d641f981d30e04abd8ac70b7a5d6b51f00a38e
LeonardoDeFaveri <leonardo.defaveri@proton.me>
2025-05-17 18:47:56 +0200

    Added recognition of dask internal management of xarray functions

--- prov_tracking/plugin.py
+++ prov_tracking/plugin.py
@@ -1,17 +1,18 @@
 from distributed.diagnostics.plugin import SchedulerPlugin
-from distributed.scheduler import Scheduler, TaskStateState as SchedulerTaskState
+from distributed.scheduler import Scheduler, TaskState, TaskStateState as SchedulerTaskState
 from dask.typing import Key
 from prov_tracking.documenter import Documenter
 from prov.serializers.provjson import encode_json_document
 import inspect
 import io
 
+from distributed.worker import execute_task
+
 class ProvTracker(SchedulerPlugin):
   """Prov tracking plugin"""
 
   def __init__(self, **kwargs):
     self.documenter = Documenter(**kwargs)
-    self.pendingActivities: dict[Key, list[str]] = {}
     self.kwargs = kwargs
 
   def start(self, scheduler: Scheduler):
@@ -22,39 +23,103 @@ class ProvTracker(SchedulerPlugin):
     *args, **kwargs
   ):
     task = self._scheduler.tasks[key]
+#    func = task.run_spec[0]
+#    f_args = task.run_spec[1]
+#    f_kwargs = task.run_spec[2]
+#    args_dict = {}
+#    param_names = list(inspect.signature(func).parameters)
+#    for name, value in zip(param_names, f_args):
+#      args_dict[name] = value
+#    args_dict.update(f_kwargs)
+#    print(f'''------------------------------------------------------------------
+#{start} -> {finish}: {key} = {{
+#  f: {func},
+#  args: {args_dict},
+#  prefix: {task.prefix},
+#  group_key: {task.group_key}
+#  group: {task.group}
+#  dependencies: {task.dependencies}
+#  dependents: {task.dependents},
+#  result: {task.type} ({task.nbytes})
+#}}''')
     # A new task has been assigned to a worker for compute: register the activity
     # and other related data
     if start == 'processing':
-      print(f'new task: {key}')
-      # Registers arguments and activity
       func = task.run_spec[0]
-      args = task.run_spec[1]
-      kwargs = task.run_spec[2]
+      f_args = task.run_spec[1]
+      f_kwargs = task.run_spec[2]
+      if ProvTracker._is_dask_internal(task):
+        func, f_args, f_kwargs = self._track_dask_internal(task)
+
       args_dict = {}
       param_names = list(inspect.signature(func).parameters)
-      for name, value in zip(param_names, args):
+      for name, value in zip(param_names, f_args):
         args_dict[name] = value
-      args_dict.update(kwargs)
+      args_dict.update(f_kwargs)
       try:
-        act, args = self.documenter.register_function_call(
-          key, func.__name__, func.__module__,
+        activity, used_params = self.documenter.register_function_call(
+          str(key), func.__name__, func.__module__,
           args_dict, task.group_key, task.dependencies
         )
       except Exception as e:
         print(f'Task {key}: {e}')
-      self.pendingActivities.setdefault(act, args)
     elif start == 'memory':
       t = task.type
       size = task.nbytes
-      self.documenter.register_function_result(key, t, size)
-      self.pendingActivities.pop(key)
+      self.documenter.register_function_result(str(key), t, size)
     elif start == 'erred':
       pass
     else:
       pass
-      
 
   async def close(self):
     ser_str = self.documenter.serialize(**self.kwargs)
     if 'destination' not in self.kwargs:
       print(ser_str)
+
+  @staticmethod
+  def _is_dask_internal(task: TaskState) -> bool:
+    func = task.run_spec[0]
+    return (
+      func.__name__ == 'execute_task' and
+      func.__module__ == 'distributed.worker'
+    )
+
+  def _track_dask_internal(self, task: TaskState):
+    # Execute task has always 1 arg, so this is safe
+    internal_func = task.run_spec[1][0]
+
+    if isinstance(internal_func, tuple):
+      func_id = internal_func[0]
+
+      if isinstance(func_id, str):
+        # internal_func is actually the id of another task, so retrieve its info        
+        task = self._scheduler.tasks[internal_func]
+        func = task.run_spec[0]
+        f_args = task.run_spec[1]
+        f_kwargs = task.run_spec[2]
+        return (func, f_args, f_kwargs)
+      elif callable(func_id):
+        # Maybe here we can avoid all checks, but some test are required
+        if func_id.__module__ == 'functools':
+          # This is most probably a functools.partial function
+          # Take the actual function being executed and its arguments
+          func = func_id.func
+          f_args = list(func_id.args)
+          f_kwargs = func_id.keywords
+          other_args = internal_func[1]
+          if (isinstance(other_args, dict)):
+            f_kwargs.update(other_args)
+          else:
+            f_args.extend(list(other_args))
+          return (func, f_args, f_kwargs)
+        else:
+          return (func_id, task.run_spec[1], task.run_spec[2])
+    else:
+      # func is an object os some kind
+      if not callable(internal_func):
+        # Since the object is not callable, keep execute_task as function so
+        # just return the original specs
+        return tuple(task.run_spec)
+
+    return (internal_func, (), {})
